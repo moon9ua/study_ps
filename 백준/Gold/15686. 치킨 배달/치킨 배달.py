9a0ec3cda5fc6d88b482e746476b0e5632a4f3bc
moon9ua <59914775+moon9ua@users.noreply.github.com>
2023-04-01 22:21:50 +0900

    [Gold V] Title: 치킨 배달, Time: 604 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.py
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.py
@@ -4,6 +4,7 @@ input = sys.stdin.readline
 
 n, m = map(int, input().split())
 board = [list(map(int, input().split())) for _ in range(n)]
+
 home = []
 ch = []
 for i in range(n):
@@ -12,15 +13,15 @@ for i in range(n):
             home.append((i,j))
         elif board[i][j] == 2:
             ch.append((i,j))
-            
-ret = 1e9
-for cb in combinations(ch, m):
-    tot_len = 0
-    for (x1,y1) in home:    
-        ch_len = 1e9
-        for (x2,y2) in cb:
-            ch_len = min(ch_len, abs(x1-x2) + abs(y1-y2))
-        tot_len += ch_len
-    ret = min(ret, tot_len)
-    
-print(ret)
+
+mn = 1e9
+for c in combinations(ch, m):
+    tot = 0
+    for x1,y1 in home:
+        dist = 1e9
+        for x2,y2 in c:
+            dist = min(dist, abs(x1-x2)+abs(y1-y2))
+        tot += dist
+    mn = min(mn, tot)
+
+print(mn)
