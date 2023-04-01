3c7266a2c97d916ef9d2f40438d750dd93523632
moon9ua <59914775+moon9ua@users.noreply.github.com>
2023-04-01 14:54:50 +0900

    [Silver II] Title: 스타트와 링크, Time: 1908 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
@@ -5,18 +5,33 @@ input = sys.stdin.readline
 n = int(input())
 mat = [list(map(int, input().split())) for _ in range(n)]
 
-mn = 1e9
-for c in combinations(range(n), n//2):
-    start = set(c)
-    link = set(range(n)) - start
-    tot_s = 0
-    tot_l = 0
-    for x,y in combinations(start, 2):
-        tot_s += mat[x][y]
-        tot_s += mat[y][x]
-    for x,y in combinations(link, 2):
-        tot_l += mat[x][y]
-        tot_l += mat[y][x]
-    mn = min(mn, abs(tot_s - tot_l))
+ans = 1e9
+start = []
+link = []
 
-print(mn)
+def backtracking(i):
+    global ans
+
+    if i == n:
+        ans = min(ans, abs(calc_status(start)-calc_status(link)))
+        return
+
+    if len(start) < n//2:
+        start.append(i)
+        backtracking(i+1)
+        start.pop()
+
+    if len(link) < n//2:
+        link.append(i)
+        backtracking(i+1)
+        link.pop()
+
+def calc_status(team):
+    status = 0
+    for i,j in combinations(team, 2):
+        status += mat[i][j]
+        status += mat[j][i]
+    return status
+
+backtracking(0)
+print(ans)
