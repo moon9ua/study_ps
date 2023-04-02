a3e89e5290bfc4048d97aadd9b619772c8a8137a
moon9ua <59914775+moon9ua@users.noreply.github.com>
2023-04-03 01:55:29 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1112 ms, Memory: 106000 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -1,12 +1,15 @@
-<html>
-<head><title>504 Gateway Time-out</title></head>
-<body>
-<center><h1>504 Gateway Time-out</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+n,m = map(int, input().split())
+board = [list(map(int, input().split())) for _ in range(n)]
+
+ps = [[0]*(n+1) for _ in range(n+1)]
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        ps[i][j] = board[i-1][j-1] + ps[i-1][j] + ps[i][j-1] - ps[i-1][j-1]
+
+for t in range(m):
+    x1,y1,x2,y2 = map(int, input().split())
+    print(ps[x2][y2] - ps[x1-1][y2] - ps[x2][y1-1] + ps[x1-1][y1-1])
