e5c2858d37bbba40799ae3098216bc10e63ccff2
moon9ua <59914775+moon9ua@users.noreply.github.com>
2023-04-02 19:52:49 +0900

    [Gold III] Title: 다리 만들기, Time: 144 ms, Memory: 34264 KB -BaekjoonHub

--- 백준/Gold/2146. 다리 만들기/다리 만들기.py
+++ 백준/Gold/2146. 다리 만들기/다리 만들기.py
@@ -1,70 +1,61 @@
 from collections import deque
 import sys
-
 input = sys.stdin.readline
 dx = [1,0,-1,0]
 dy = [0,1,0,-1]
 
 n = int(input())
 board = [list(map(int, input().split())) for _ in range(n)]
-color = [[0]*n for _ in range(n)]
 
-num = 1
+vis = [[False]*n for _ in range(n)]
+color = 0
+
+q_bridge = deque()
+dist = [[-1]*n for _ in range(n)]
+
 for i in range(n):
     for j in range(n):
-        if not color[i][j] and board[i][j]:
-            color[i][j] = num
+        if board[i][j] == 1 and not vis[i][j]:
             q = deque()
-            q.append((i,j))
+            color += 1
+
+            q.append((i, j))
+            vis[i][j] = True
+            board[i][j] = color
+
             while q:
-                x,y = q.popleft()
+                x, y = q.popleft()
                 for a in range(4):
                     nx = x + dx[a]
                     ny = y + dy[a]
                     if nx < 0 or ny < 0 or nx >= n or ny >= n:
                         continue
-                    if color[nx][ny] or board[nx][ny] == 0:
+                    if vis[nx][ny]:
                         continue
-                    color[nx][ny] = num
-                    q.append((nx,ny))
-        num += 1
+                    if board[nx][ny] == 0:
+                        q_bridge.append((x,y,color))
+                        dist[x][y] = 0
+                        continue
+                    q.append((nx, ny))
+                    vis[nx][ny] = True
+                    board[nx][ny] = color
 
-def check(i,j):
-    if board[i][j] == 0:
-        return False
-    zero = 0
+mn = 1e9
+
+while q_bridge:
+    x,y,c = q_bridge.popleft()
     for a in range(4):
-        ni = i + dx[a]
-        nj = j + dy[a]
-        if ni < 0 or nj < 0 or ni >= n or nj >= n:
+        nx = x + dx[a]
+        ny = y + dy[a]
+        if nx < 0 or ny < 0 or nx >= n or ny >= n:
             continue
-        if board[ni][nj]:
+        if board[nx][ny] == c:
             continue
-        zero += 1
-    return bool(zero)
+        if board[nx][ny] == 0:
+            q_bridge.append((nx,ny,c))
+            dist[nx][ny] = dist[x][y] + 1
+            board[nx][ny] = c
+        else:
+            mn = min(mn, dist[x][y] + dist[nx][ny])
 
-ans = 1e9
-for i in range(n):
-    for j in range(n):
-        if check(i,j):
-            now = color[i][j]
-            vis = [[-1]*n for _ in range(n)]
-            vis[i][j] = 0
-            q = deque()
-            q.append((i,j))
-            while q:
-                x,y = q.popleft()
-                if color[x][y] != 0 and color[x][y] != now:
-                    ans = min(ans, vis[x][y]-1)
-                    break
-                for a in range(4):
-                    nx = x + dx[a]
-                    ny = y + dy[a]
-                    if nx < 0 or ny < 0 or nx >= n or ny >= n:
-                        continue
-                    if color[nx][ny] == now or vis[nx][ny] != -1:
-                        continue
-                    vis[nx][ny] = vis[x][y] + 1
-                    q.append((nx,ny))
-
-print(ans)
+print(mn)
