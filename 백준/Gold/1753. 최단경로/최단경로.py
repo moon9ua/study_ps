ef454dbc83c328b1c3e4751b7a3cfaa755b900e8
moon9ua <59914775+moon9ua@users.noreply.github.com>
2023-04-15 01:52:30 +0900

    [Gold IV] Title: 최단경로, Time: 656 ms, Memory: 68944 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.py
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -8,23 +8,22 @@ graph = [[] for _ in range(v+1)]
 for _ in range(e):
     a,b,c = map(int,input().split())
     graph[a].append((b,c))
-dist = [1e9] * (v+1)
 
-q = []
+dist = [1e9] * (v+1)
+pq = []
+
 dist[st] = 0
-heapq.heappush(q, (0, st))
-while q:
-    d, x = heapq.heappop(q)
+heapq.heappush(pq, (0,st))
+
+while pq:
+    d,x = heapq.heappop(pq)
     if dist[x] < d:
         continue
-    for nx, nd in graph[x]:
+    for nx,nd in graph[x]:
         totd = d + nd
         if totd < dist[nx]:
             dist[nx] = totd
-            heapq.heappush(q, (totd, nx))
+            heapq.heappush(pq, (totd,nx))
 
-for i in range(1, v+1):
-    if dist[i] == 1e9:
-        print('INF')
-    else:
-        print(dist[i])
+ret = ['INF' if d==1e9 else d for d in dist]
+print(*ret[1:], sep='\n')
