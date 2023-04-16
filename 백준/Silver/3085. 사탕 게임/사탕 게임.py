684e9f9359c0e76f1cdbcab400a9a39ae56f0c53
moon9ua <59914775+moon9ua@users.noreply.github.com>
2023-04-17 00:57:54 +0900

    [Silver II] Title: 사탕 게임, Time: 1992 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/3085. 사탕 게임/사탕 게임.py
@@ -0,0 +1,43 @@
+import sys
+input = sys.stdin.readline
+dx = [1,0,-1,0]
+dy = [0,1,0,-1]
+
+n = int(input())
+board = [input().rstrip() for _ in range(n)]
+
+def search():
+    ret = 0
+    for i in range(n):
+        cntX, cntY = 0,0
+        lenX, lenY = 1,1
+        for j in range(1, n):
+            if tmp[i][j-1] == tmp[i][j]:
+                lenX += 1
+                cntX = max(cntX, lenX)
+            else:
+                lenX = 1
+
+            if tmp[j-1][i] == tmp[j][i]:
+                lenY += 1
+                cntY = max(cntY, lenY)
+            else:
+                lenY = 1
+        ret = max(ret, max(cntX, cntY))
+    return ret
+
+cnt = 0
+tmp = [list(row) for row in board]
+for i in range(n):
+    for j in range(n):
+        if i < n-1 and tmp[i][j] != tmp[i+1][j]:
+            tmp[i][j], tmp[i+1][j] = tmp[i+1][j], tmp[i][j]
+            cnt = max(cnt, search())
+            tmp[i][j], tmp[i+1][j] = tmp[i+1][j], tmp[i][j] 
+
+        if j < n-1 and tmp[i][j] != tmp[i][j+1]:
+            tmp[i][j], tmp[i][j+1] = tmp[i][j+1], tmp[i][j]
+            cnt = max(cnt, search())
+            tmp[i][j], tmp[i][j+1] = tmp[i][j+1], tmp[i][j]
+
+print(cnt)
