44f72ff3bb09209df45060ae1882e11511fe06ed
moon9ua <59914775+moon9ua@users.noreply.github.com>
2023-06-06 00:25:02 +0900

    [Gold IV] Title: 거짓말, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/1043. 거짓말/거짓말.py
+++ 백준/Gold/1043. 거짓말/거짓말.py
@@ -2,24 +2,40 @@ import sys
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
-_, *lst = list(map(int, input().split()))
+_, *truth = list(map(int, input().split()))
 
-tr = set(lst)
-pts = []
+know_truth = 0
+p = [i for i in range(n+1)]
+for person in truth:
+    p[person] = know_truth
 
+def find(x):
+    if p[x] != x:
+        p[x] = find(p[x])
+    return p[x]
+
+def union(a, b):
+    a = find(a)
+    b = find(b)
+    if a < b:
+        p[b] = a
+    else:
+        p[a] = b
+
+parties = []
 for _ in range(m):
-    _, *lst = list(map(int, input().split()))
-    pt = set(lst)
-    pts.append(pt)
-
-for _ in range(m):
-    for pt in pts:
-        if tr & pt:
-            tr = tr | pt
+    k, *party = list(map(int, input().split()))
+    parties.append(party)
+    for i in range(k-1):
+        union(party[i], party[i+1])
 
 cnt = 0
-for p in pts:
-    if not tr & p:
-        cnt += 1
+for party in parties:
+    tmp = 1
+    for person in party:
+        if find(person) == know_truth:
+            tmp = 0
+            break
+    cnt += tmp
 
 print(cnt)
