a5829c3a00040026a317f8a8e371ea89538ea251
moon9ua <59914775+moon9ua@users.noreply.github.com>
2023-06-07 00:23:08 +0900

    [Gold III] Title: 웜홀, Time: 904 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/1865. 웜홀/웜홀.py
+++ 백준/Gold/1865. 웜홀/웜홀.py
@@ -1,57 +1,30 @@
-# 통과 테스트용 제출
 import sys
-
 input = sys.stdin.readline
-INF = int(1e9)
 
-
-# 벨만 포드 알고리즘 응용 -> 그래프상 음의 싸이클 존재 판단 함수
-def bf(start):
-    dis = [INF] * (n+1)  # 최단거리 초기화
-    dis[start]=0
-    # 메인 로직
-    # 음의 사이클 판별을 위해 n번 반복
-    for i in range(n):
-        # 반복마다 모든 간선 확인
+def bf(st):
+    dist[st] = 0
+    for i in range(N):
         for edge in edges:
-            cur = edge[0] # 출발
-            next_node = edge[1] # 도착
-            cost = edge[2] # 비용
-
-            
-            # 다음 노드로 이동하는 거리가 최단거리로 갱신가능한 경우
-            if dis[next_node] > cost + dis[cur]:
-                dis[next_node] = cost + dis[cur]
-                # i==n-1이면 n번 돌린건데 이때도 갱신이 발생하면 음의 싸이클 존재
-                if i == n - 1:
-                    return True
-
+            cur, nxt, cost = edge
+            # if dist[cur] != 1e9 and dist[nxt] > dist[cur] + cost:
+            if dist[nxt] > dist[cur] + cost:
+                dist[nxt] = dist[cur] + cost
+                if i == N-1:
+                    return True # 음수 사이클 존재
     return False
 
-
-t = int(input())
-
-for _ in range(t):
-    # 지점수, 도로수, 웜홀수
-    n, m, w = map(int, input().split())
+TC = int(input())
+for _ in range(TC):
+    N, M, W = map(int, input().split())
+    dist = [1e9] * (N+1)
     edges = []
+    for _ in range(M):
+        a, b, c = map(int, input().split())
+        edges.append((a, b, c))
+        edges.append((b, a, c))
+    for _ in range(W):
+        a, b, c = map(int, input().split())
+        edges.append((a, b, -c))
 
-    # 도로 정보
-    for _ in range(m):
-        s, e, t = map(int, input().split())
-        edges.append((s, e, t))
-        edges.append((e, s, t))
-
-    # 웜홀 정보
-    for _ in range(w):
-        s, e, t = map(int, input().split())
-        edges.append((s, e, -t))
-
-    # bf알고리즘 조건에 dis[cur]!=INF 조건이 없으므로
-    # 시작 위치는 아무거나 무관
-    # bf는 최단거리 알고리즘이 아닌 음의 싸이클의 판별 유무 판단 알고리즘
-    key = bf(1)
-    if key:
-        print("YES")
-    else:
-        print("NO")
+    neg_cycle = bf(1)
+    print('YES' if neg_cycle else 'NO')
