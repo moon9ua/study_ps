c16903e378ed858d692e9cfcf256eed34132983f
moon9ua <59914775+moon9ua@users.noreply.github.com>
2023-06-09 00:35:14 +0900

    [Gold IV] Title: 알파벳, Time: 5080 ms, Memory: 167956 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.py
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -6,23 +6,24 @@ dy = [0, 1, 0, -1]
 r, c = map(int, input().split())
 board = [input().rstrip() for _ in range(r)]
 
-def dfs(x, y):
-    global mx, path
+def dfs(x, y, k):
+    global mx
 
     for a in range(4):
         nx = x + dx[a]
         ny = y + dy[a]
         if nx < 0 or ny < 0 or nx >= r or ny >= c:
             continue
-        if board[nx][ny] in path:
+        if used[ord(board[nx][ny])]:
             continue
-        path += board[nx][ny]
-        mx = max(mx, len(path))
-        dfs(nx, ny)
-        path = path[:-1]
+        used[ord(board[nx][ny])] = True
+        mx = max(mx, k+1)
+        dfs(nx, ny, k+1)
+        used[ord(board[nx][ny])] = False
     
+used = [False] * 200
+used[ord(board[0][0])] = True
 mx = 1
-path = board[0][0]
-dfs(0, 0)
+dfs(0, 0, 1)
 
 print(mx)
