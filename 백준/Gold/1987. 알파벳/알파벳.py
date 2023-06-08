750dce24d312d84e889b6c9b86ecb6eb31b440d0
moon9ua <59914775+moon9ua@users.noreply.github.com>
2023-06-09 00:23:14 +0900

    [Gold IV] Title: 알파벳, Time: 6956 ms, Memory: 165536 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.py
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -1,36 +1,28 @@
-from collections import deque
 import sys
-
 input = sys.stdin.readline
-dx = [1,0,-1,0]
-dy = [0,1,0,-1]
+dx = [1, 0, -1, 0]
+dy = [0, 1, 0, -1]
 
-r,c = map(int, input().split())
+r, c = map(int, input().split())
 board = [input().rstrip() for _ in range(r)]
 
-vis = [[False]*c for _ in range(r)]
-used = [False] * 200
-mx = 1
+def dfs(x, y):
+    global mx, path
 
-def func(x,y,k):
-    global mx
-    
     for a in range(4):
         nx = x + dx[a]
         ny = y + dy[a]
         if nx < 0 or ny < 0 or nx >= r or ny >= c:
             continue
-        if vis[nx][ny] or used[ord(board[nx][ny])]:
+        if board[nx][ny] in path:
             continue
-        vis[nx][ny] = True
-        used[ord(board[nx][ny])] = True
-        mx = max(mx, k+1)
-        func(nx,ny,k+1)
-        vis[nx][ny] = False
-        used[ord(board[nx][ny])] = False
-
-vis[0][0] = True
-used[ord(board[0][0])] = True
-func(0,0,1)
+        path += board[nx][ny]
+        mx = max(mx, len(path))
+        dfs(nx, ny)
+        path = path[:-1]
+    
+mx = 1
+path = board[0][0]
+dfs(0, 0)
 
 print(mx)
