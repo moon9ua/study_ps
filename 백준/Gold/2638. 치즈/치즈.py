b1b271a5e1a56e47b90d1704075bc9dfbe5b7238
moon9ua <59914775+moon9ua@users.noreply.github.com>
2023-06-17 16:16:42 +0900

    [Gold III] Title: 치즈, Time: 92 ms, Memory: 34192 KB -BaekjoonHub

--- 백준/Gold/2638. 치즈/치즈.py
+++ 백준/Gold/2638. 치즈/치즈.py
@@ -1,59 +1,52 @@
-from collections import deque
+from collections import deque, defaultdict
 import sys
 input = sys.stdin.readline
-dx = [1,0,-1,0]
-dy = [0,1,0,-1]
 
-n,m = map(int, input().split())
-board = [list(map(int, input().split())) for _ in range(n)]
+dx = [1, 0, -1, 0]
+dy = [0, 1, 0, -1]
 
-cnt = 0
-for i in range(n):
-    for j in range(m):
-        if board[i][j] == 1:
-            cnt += 1
+n, m = map(int, input().split())
+board = []
+tot = 0
+for _ in range(n):
+    row = list(map(int, input().split()))
+    board.append(row)
+    tot += row.count(1)
 
-def check(x, y, time):
-    cnt = 0
-    for a in range(4):
-        nx = x + dx[a]
-        ny = y + dy[a]
-        if 0 <= vis[nx][ny] < time:
-            cnt += 1
-    return cnt >= 2
 
 q = deque()
-vis = [[-1] * m for _ in range(n)]
+vis = [[False] * m for _ in range(n)]
 
-q.append((0,0))
-vis[0][0] = 0
-time = 0
+q.append((0, 0))
+vis[0][0] = True
 
-while cnt:
-    nxt = set()
-    
+cheeze = defaultdict(int)
+cnt = 0
+
+while tot:
+    cnt += 1
+
     while q:
-        x,y = q.popleft()
+        x, y = q.popleft()
         for a in range(4):
             nx = x + dx[a]
             ny = y + dy[a]
             if nx < 0 or ny < 0 or nx >= n or ny >= m:
                 continue
-            if vis[nx][ny] != -1:
+            if vis[nx][ny]:
                 continue
-            if board[nx][ny] == 0:
-                q.append((nx,ny))
-                vis[nx][ny] = time
+            if board[nx][ny] == 1:
+                cheeze[(nx, ny)] += 1
             else:
-                nxt.add((nx,ny))
-    
-    time += 1
+                q.append((nx, ny))
+                vis[nx][ny] = True
 
-    for x,y in nxt:
-        if check(x, y, time):
-            q.append((x,y))
-            vis[x][y] = time
+    for (x, y), v in list(cheeze.items()):
+        if v >= 2:
             board[x][y] = 0
-            cnt -= 1
+            tot -= 1
+            cheeze.pop((x, y))
+            q.append((x, y))
+            vis[x][y] = True
 
-print(time)
+print(cnt)
