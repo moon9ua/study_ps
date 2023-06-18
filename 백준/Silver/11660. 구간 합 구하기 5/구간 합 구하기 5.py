1da4af5ebf0ea9e62995d06f8acfea76bd70bdc7
moon9ua <59914775+moon9ua@users.noreply.github.com>
2023-06-18 22:21:42 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1076 ms, Memory: 106004 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -1,8 +1,7 @@
-from collections import deque
 import sys
 input = sys.stdin.readline
 
-n,m = map(int, input().split())
+n, m = map(int, input().split())
 board = [list(map(int, input().split())) for _ in range(n)]
 
 ps = [[0]*(n+1) for _ in range(n+1)]
@@ -10,6 +9,7 @@ for i in range(1, n+1):
     for j in range(1, n+1):
         ps[i][j] = board[i-1][j-1] + ps[i-1][j] + ps[i][j-1] - ps[i-1][j-1]
 
-for t in range(m):
-    x1,y1,x2,y2 = map(int, input().split())
-    print(ps[x2][y2] - ps[x1-1][y2] - ps[x2][y1-1] + ps[x1-1][y1-1])
+for _ in range(m):
+    x1, y1, x2, y2 = map(int, input().split()) # 1-idx
+    ans = ps[x2][y2] - ps[x1-1][y2] - ps[x2][y1-1] + ps[x1-1][y1-1]
+    print(ans)
