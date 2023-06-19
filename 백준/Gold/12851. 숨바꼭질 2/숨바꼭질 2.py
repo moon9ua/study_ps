a0f848b3aceb1e6e9702e9ed90544819db6dc19d
moon9ua <59914775+moon9ua@users.noreply.github.com>
2023-06-19 23:41:11 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 160 ms, Memory: 35892 KB -BaekjoonHub

--- 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.py
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.py
@@ -4,24 +4,25 @@ n, k = map(int, input().split())
 
 q = deque()
 vis = [-1] * 100005
+cnt = [0] * 100005
 
 q.append(n)
 vis[n] = 0
+cnt[n] = 1
 
-ans = 0
-cnt = 0
 while q:
     x = q.popleft()
     if x == k:
-        cnt += 1
         continue
     for nx in (x-1, x+1, x*2):
         if nx < 0 or nx > 100000:
             continue
-        if vis[nx] != -1 and vis[nx] != vis[x] + 1:
-            continue
-        q.append(nx)
-        vis[nx] = vis[x] + 1
+        if vis[nx] == -1:
+            q.append(nx)
+            vis[nx] = vis[x] + 1
+            cnt[nx] = cnt[x]
+        elif vis[nx] == vis[x] + 1:
+            cnt[nx] += cnt[x]
 
 print(vis[k])
-print(cnt)
+print(cnt[k])
