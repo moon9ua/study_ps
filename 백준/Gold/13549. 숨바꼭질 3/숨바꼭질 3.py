3c3e6a7f86c28dca3e0851d0abc4f6b192b5b0c2
moon9ua <59914775+moon9ua@users.noreply.github.com>
2023-06-20 23:12:22 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 128 ms, Memory: 35360 KB -BaekjoonHub

---
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -0,0 +1,31 @@
+from collections import deque
+import sys
+
+input = sys.stdin.readline
+
+n,k = map(int,input().split())
+dist = [-1] * 100005
+
+dist[n] = 0
+q = deque()
+q.append(n)
+
+while q:
+    x = q.popleft()
+    if x == k:
+        break
+
+    nx = x*2
+    if (0 <= nx <= 100000) and dist[nx] == -1:
+        dist[nx] = dist[x]
+        q.appendleft(nx)
+
+    for nx in [x-1,x+1]:
+        if nx < 0 or nx > 100000:
+            continue
+        if dist[nx] != -1:
+            continue
+        dist[nx] = dist[x]+1
+        q.append(nx)
+
+print(dist[k])
