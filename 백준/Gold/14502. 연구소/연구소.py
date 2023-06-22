f21f3c73650c569bdee01fa441a59db5dd587fd6
moon9ua <59914775+moon9ua@users.noreply.github.com>
2023-06-22 22:56:31 +0900

    [Gold IV] Title: 연구소, Time: 444 ms, Memory: 117156 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -1,66 +1,48 @@
-# combination 사용하지 않고 재귀로 최적화 시도
-
+from itertools import combinations
 from collections import deque
 import sys
 input = sys.stdin.readline
-dx = [1, 0, -1, 0]
-dy = [0, 1, 0, -1]
+dx = [1,0,-1,0]
+dy = [0,1,0,-1]
 
 n, m = map(int, input().split())
 board = [list(map(int, input().split())) for _ in range(n)]
 
 empty = []
 virus = []
-area = 0
 for i in range(n):
     for j in range(m):
         if board[i][j] == 0:
-            empty.append((i, j))
-            area += 1
+            empty.append((i,j))
         elif board[i][j] == 2:
-            virus.append((i, j))
-area -= 3
+            virus.append((i,j))
 
-def bfs():
-    q = deque(virus)
-    vis = [[False]*m for _ in range(n)]
-    cnt = area
+ans = -1e9
+for cb in combinations(empty, 3):
+    q = deque()
+    vis = [[False] * m for _ in range(n)]
 
-    for x, y in virus:
-        vis[x][y] = True
+    for new_x, new_y in cb:
+        vis[new_x][new_y] = True
 
-    while q:
-        x, y = q.popleft()
-        for a in range(4):
-            nx = x + dx[a]
-            ny = y + dy[a]
-            if nx < 0 or ny < 0 or nx >= n or ny >= m:
-                continue
-            if board[nx][ny] != 0 or vis[nx][ny]:
-                continue
-            q.append((nx, ny))
-            vis[nx][ny] = True
-            cnt -= 1
+    area = len(empty) - 3
 
-    return cnt
+    for vx,vy in virus:
+        q.append((vx,vy))
+        vis[vx][vy] = True
+        while q:
+            x,y = q.popleft()
+            for a in range(4):
+                nx = x + dx[a]
+                ny = y + dy[a]
+                if nx < 0 or ny < 0 or nx >= n or ny >= m:
+                    continue
+                if board[nx][ny] != 0 or vis[nx][ny]:
+                    continue
+                q.append((nx,ny))
+                vis[nx][ny] = True
+                area -= 1
 
-mx = 0
-used = [False] * 100
-def func(st, k):
-    global mx
+    ans = max(ans, area)
 
-    if k == 3:
-        mx = max(mx, bfs())
-        return
-
-    for i in range(st, len(empty)):
-        if not used[i]:
-            x, y = empty[i]
-            used[i] = True
-            board[x][y] = 1
-            func(i, k+1)
-            used[i] = False
-            board[x][y] = 0
-        
-func(0, 0)
-print(mx)
+print(ans)
