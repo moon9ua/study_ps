984d3f73afc75c11749c39960ed790a21c5ddeda
moon9ua <59914775+moon9ua@users.noreply.github.com>
2023-06-24 22:28:25 +0900

    [Gold III] Title: 아기 상어, Time: 172 ms, Memory: 34264 KB -BaekjoonHub

--- 백준/Gold/16236. 아기 상어/아기 상어.py
+++ 백준/Gold/16236. 아기 상어/아기 상어.py
@@ -1,74 +1,64 @@
-from itertools import combinations
 from collections import deque
 import sys
 input = sys.stdin.readline
-dx = [-1,0,1,0]
-dy = [0,-1,0,1]
 
 n = int(input())
-board = [list(map(int, input().split())) for _ in range(n)]
-
-st = None
-food_cnt = 0
-
+board = []
+food = [0] * 10
+cur = None
 for i in range(n):
+    row = list(map(int, input().split()))
+    board.append(row)
     for j in range(n):
-        if board[i][j] == 0:
-            continue
-        elif board[i][j] == 9:
-            st = (i,j)
+        if board[i][j] == 9:
+            cur = (i, j)
             board[i][j] = 0
-        else:
-            food_cnt += 1
+        elif board[i][j] != 0:
+            food[board[i][j]] += 1
 
 time = 0
 size = 2
 rest = size
-
-def calc_next(next_points):
-    ans = next_points[0]
-    for p in next_points:
-        if p[0] < ans[0]:
-            ans = p
-        elif p[0] == ans[0] and p[1] < ans[1]:
-            ans = p
-    return ans
-
-while food_cnt:
+d = ((-1,0), (0,-1), (0,1), (1,0))
+while sum(food[:size]):
     q = deque()
-    dist = [[-1]*n for _ in range(n)]
-    next_points = []
-    min_dist = 1e9
-    
-    q.append(st)
-    dist[st[0]][st[1]] = 0
-    while q and food_cnt:
-        x,y = q.popleft()
-        for a in range(4):
-            nx = x + dx[a]
-            ny = y + dy[a]
+    vis = [[-1]*n for _ in range(n)]
+
+    q.append(cur)
+    vis[cur[0]][cur[1]] = 0
+
+    nxt_d = 1e9
+    nxt_lst = []
+    while q:
+        x, y = q.popleft()
+        if board[x][y] and board[x][y] < size:
+            if nxt_d == 1e9:
+                nxt_d = vis[x][y]
+                nxt_lst.append((x, y))
+            elif nxt_d == vis[x][y]:
+                nxt_lst.append((x, y))
+            continue
+        for dx, dy in d:
+            nx = x + dx
+            ny = y + dy
             if nx < 0 or ny < 0 or nx >= n or ny >= n:
                 continue
-            if dist[nx][ny] != -1 or board[nx][ny] > size:
+            if board[nx][ny] > size or vis[nx][ny] != -1:
                 continue
-            dist[nx][ny] = dist[x][y] + 1
-            if board[nx][ny] and board[nx][ny] < size and min_dist >= dist[nx][ny]:
-                min_dist = dist[nx][ny]
-                next_points.append((nx,ny))
-                continue
-            q.append((nx,ny))
+            q.append((nx, ny))
+            vis[nx][ny] = vis[x][y] + 1
 
-    if not next_points:
+    if not nxt_lst:
         break
 
-    nx, ny = calc_next(next_points)
-    time += dist[nx][ny]
-    st = (nx, ny)
-    food_cnt -= 1
+    nx, ny = max(nxt_lst, key=lambda x: (-x[0], -x[1]))
     rest -= 1
-    if not rest:
+    if rest == 0:
         size += 1
         rest = size
+    food[board[nx][ny]] -= 1
+    time += vis[nx][ny]
+    cur = (nx, ny)
     board[nx][ny] = 0
-
+        
 print(time)
