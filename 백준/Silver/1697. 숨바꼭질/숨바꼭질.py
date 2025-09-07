4ee4166aae76ae2bbcb698428af9d92cf15f2768
moon9ua <moon9ua@gmail.com>
2025-09-07 16:15:30 +0900

    [Silver I] Title: 숨바꼭질, Time: 140 ms, Memory: 37364 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -1,28 +1,32 @@
+### sys input ###
+
+import sys
+
+input = sys.stdin.readline
+
+### solve ###
+
 from collections import deque
 
-n, k = map(int, input().split())
-dist = [-1] * 100005
-q = deque()
-dist[n] = 0
-q.append(n)
+N, K = map(int, input().split())
 
-def process():
-    global n, k
-    
-    if n == k:
-        return 0
-    
-    while q:
-        x = q.popleft()
-        
-        for nx in [x-1, x+1, x*2]:
-            if nx < 0 or nx > 100000:
-                continue
-            if dist[nx] != -1:
-                continue
-            if nx == k:
-                return dist[x] + 1
-            dist[nx] = dist[x] + 1
-            q.append(nx)
-        
-print(process())
+vis = [-1] * 100004
+q = deque()
+
+vis[N] = 0
+q.append(N)
+
+while q:
+    x = q.popleft()
+    if x == K:
+        break
+
+    for nx in (x - 1, x + 1, 2 * x):
+        if nx < 0 or nx > 100000:
+            continue
+        if vis[nx] != -1:
+            continue
+        vis[nx] = vis[x] + 1
+        q.append(nx)
+
+print(vis[K])
