cb3db91ce0d747ae10b93902bb60f9c5fa7b555a
moon9ua <moon9ua@gmail.com>
2025-09-07 23:46:29 +0900

    [Silver III] Title: 1로 만들기, Time: 524 ms, Memory: 71968 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,13 +1,22 @@
-n = int(input())
+### sys input ###
 
-dp = [0] * 1000005
-dp[1] = 0
+import sys
 
-for i in range(2, n+1):
-    dp[i] = dp[i-1] + 1
-    if i % 2 == 0:
-        dp[i] = min(dp[i], dp[i//2]+1)
-    if i % 3 == 0:
-        dp[i] = min(dp[i], dp[i//3]+1)
+input = sys.stdin.readline
 
-print(dp[n])
+### solve ###
+
+N = int(input())
+
+dp = [1e9] * (N + 1)
+dp[N] = 0
+
+for i in range(N - 1, 0, -1):
+    dp[i] = dp[i + 1] + 1
+
+    if i * 3 <= N:
+        dp[i] = min(dp[i], dp[i * 3] + 1)
+    if i * 2 <= N:
+        dp[i] = min(dp[i], dp[i * 2] + 1)
+
+print(dp[1])
