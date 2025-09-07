291f5df9c3cb420e136669f5edae73ae67803a1b
moon9ua <moon9ua@gmail.com>
2025-09-07 23:24:22 +0900

    [Silver III] Title: 피보나치 함수, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
@@ -1,22 +1,35 @@
+### sys input ###
+
 import sys
+
 input = sys.stdin.readline
 
-arr = [[0] * 42 for _ in range(2)]
-arr[0][0] = 1
-arr[1][0] = 0
-arr[0][1] = 0
-arr[1][1] = 1
+### solve ###
 
-'''
-arr[0][n] =
-arr[1][n] = 
-'''
 
-for i in range(2, 41):
-    arr[0][i] = arr[0][i-1] + arr[0][i-2]
-    arr[1][i] = arr[1][i-1] + arr[1][i-2]
+def solve(N):
+    if N == 0:
+        return (1, 0)
+    if N == 1:
+        return (0, 1)
 
-t = int(input())
-for _ in range(t):
-    n = int(input())
-    print(arr[0][n], arr[1][n])
+    dp0 = [0] * (N + 1)
+    dp0[0] = 1
+    dp0[1] = 0
+
+    dp1 = [0] * (N + 1)
+    dp1[0] = 0
+    dp1[1] = 1
+
+    for i in range(2, N + 1):
+        dp0[i] = dp0[i - 1] + dp0[i - 2]
+        dp1[i] = dp1[i - 1] + dp1[i - 2]
+
+    return (dp0[N], dp1[N])
+
+
+T = int(input())
+
+for _ in range(T):
+    N = int(input())
+    print(*solve(N))
